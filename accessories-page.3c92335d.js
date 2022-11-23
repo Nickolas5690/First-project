ce9566fb7c76de73ce03e66ee0b28df005f3b777
Nickolas5690 <113071344+Nickolas5690@users.noreply.github.com>
2022-11-23 11:00:42 +0000

    Deploying to gh-pages from @ Nickolas5690/First-project@113fb42d6f2f07df1567ba95971b9a6adfd80809 🚀

---
+++ accessories-page.3c92335d.js
@@ -0,0 +1,2 @@
+!function(){var e,t;e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),e.addEventListener("click",(function(){var a="true"===e.getAttribute("aria-epanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-epanded",!a),t.classList.toggle("is-hidden")}))}();
+//# sourceMappingURL=accessories-page.3c92335d.js.map
---
+++ accessories-page.631618a9.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(()=>{const a="true"===e.getAttribute("aria-epanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-epanded",!a),t.classList.toggle("is-hidden")}))})();
+//# sourceMappingURL=accessories-page.631618a9.js.map
