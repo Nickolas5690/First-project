4155f13e3576782cbc6a1d74ac922feab9a603ff
Nickolas5690 <113071344+Nickolas5690@users.noreply.github.com>
2022-11-23 11:28:24 +0000

    Deploying to gh-pages from @ Nickolas5690/First-project@8bf31cd31022dbb44535e8f508342db8fcdc00dc 🚀

--- accessories-page.631618a9.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(()=>{const a="true"===e.getAttribute("aria-epanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-epanded",!a),t.classList.toggle("is-hidden")}))})();
-//# sourceMappingURL=accessories-page.631618a9.js.map
